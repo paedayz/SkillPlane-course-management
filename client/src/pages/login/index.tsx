ff7d673cae3cdfe6d07f3037ba121a3a509941c7
paedayz <pae16phasit@gmail.com>
2022-06-20 11:19:26 +0700

    created new styled for login and register page

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -5,21 +5,88 @@ import styled from "styled-components";
 import { login } from "../../api";
 import { useAppDispatch } from "../../app/hooks";
 import { setCredentials } from "../../slices/user.slice";
+import { LockFilled, UserOutlined } from "@ant-design/icons";
+import { device } from "../../constants";
 
 // styled
 const Container = styled.div`
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
   text-align: center;
+  background-color: #d9daff;
+  width: 100%;
+  height: 100vh;
 `;
 
+const BoxForm = styled.div`
+  width: 40%;
+  height: 50vh;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 30px;
+
+  @media ${device.ipad} {
+    width: 70%;
+  }
+
+  @media ${device.mobile} {
+    width: 90%;
+  }
+`;
 const Title = styled.div`
-  font-size: 20px;
-  margin-bottom: 20px;
+  font-size: 38px;
+  font-weight: bold;
+  padding-top: 8%;
 `;
 
+export const InputUsername = styled(Input)`
+  margin: 0;
+  border-radius: 30px;
+  height: 50px;
+  font-size: 24px;
+  border-color: #a2b4fa;
+  @media ${device.mobile} {
+    width: 80%;
+  }
+`;
+
+export const InputPassword = styled(Input.Password)`
+  margin: 0;
+  border-radius: 30px;
+  height: 50px;
+  font-size: 24px;
+  border-color: #a2b4fa;
+  margin-bottom: 10px;
+  @media ${device.mobile} {
+    width: 80%;
+  }
+`;
+
+export const FormItem = styled(Form.Item)`
+  .ant-form-item-label > label {
+    padding-top: 12px;
+    padding-right: 10px;
+  }
+  .ant-input {
+    font-size: 20px;
+  }
+`;
+export const ButtonLoginForm = styled(Button)`
+  border-radius: 30px;
+  background-color: #6adb89;
+  color: white;
+  height: 50px;
+  font-size: 16px;
+  margin: 0 auto;
+  display: block;
+  width: 213px;
+  font-weight: bolder;
+  font-size: 20px;
+`;
 interface IFormInput {
   username?: string;
   password?: string;
@@ -28,59 +95,47 @@ interface IFormInput {
 type Props = {};
 
 function LoginPage(props: Props) {
-    const [loading, setLoading] = useState(false)
-    const dispatch = useAppDispatch()
-    const history = useHistory()
+  const [loading, setLoading] = useState(false);
+  const dispatch = useAppDispatch();
+  const history = useHistory();
   const onFinish = async (values: IFormInput) => {
-    if(values.username && values.password) {
-        setLoading(true)
-        const res = await login(values.username, values.password)
-        if(!res) return
-        dispatch(setCredentials())
-        history.push('/')
+    if (values.username && values.password) {
+      setLoading(true);
+      const res = await login(values.username, values.password);
+      if (!res) return;
+      dispatch(setCredentials());
+      history.push("/");
     }
 
-    setLoading(false)
-    
+    setLoading(false);
   };
 
   return (
     <Container>
       <Title>Login</Title>
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input />
-        </Form.Item>
+      <BoxForm>
+        <Form initialValues={{ remember: true }} onFinish={onFinish} autoComplete="off">
+          <FormItem name="username" rules={[{ required: true, message: "Please input your username!" }]}>
+            <InputUsername placeholder="Username" prefix={<UserOutlined />} />
+          </FormItem>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
+          <FormItem name="password" rules={[{ required: true, message: "Please input your password!" }]}>
+            <InputPassword placeholder="Password" prefix={<LockFilled />} />
+          </FormItem>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 8 }}>
-          <Link to="register">Register ?</Link>
-        </Form.Item>
+          <Form.Item>
+            <Link style={{ fontSize: "20px" }} to="register">
+              Register ?
+            </Link>
+          </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 8 }}>
-          <Button disabled={loading} type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item>
+            <ButtonLoginForm disabled={loading} htmlType="submit">
+              Submit
+            </ButtonLoginForm>
+          </Form.Item>
+        </Form>
+      </BoxForm>
     </Container>
   );
 }
--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -3,20 +3,104 @@ import { Link, useHistory } from "react-router-dom";
 import styled from "styled-components";
 import { register } from "../../api";
 import { useAppDispatch } from "../../app/hooks";
+import { device } from "../../constants";
 import { setCredentials } from "../../slices/user.slice";
 
 // styled
 const Container = styled.div`
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
   text-align: center;
+  background-color: #d9daff;
+  width: 100%;
+  height: 110vh;
+  @media ${device.mobile} {
+    height: 200vh;
+  }
+`;
+
+const BoxForm = styled.div`
+  width: 40%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  border-radius: 30px;
+  padding: 0px 20px 0px 20px;
+  display: flex;
+  justify-content: center;
+  @media ${device.ipad} {
+    display: flex;
+    justify-content: center;
+    width: 70%;
+    height: 60vh;
+    margin-top: 0;
+    top: 40%;
+  }
+
+  @media ${device.mobile} {
+    position: relative;
+    width: 90%;
+    height: 160vh;
+    top: 42%;
+    display: block;
+    padding: 15px;
+  }
 `;
 
 const Title = styled.div`
+  font-size: 38px;
+  font-weight: bold;
+  padding-top: 10px;
+  z-index: 1;
+  position: relative;
+`;
+
+export const InputForm = styled(Input)`
+  border-radius: 30px;
+  height: 45px;
+  font-size: 24px;
+  border-color: #a2b4fa;
+`;
+
+export const InputPassword = styled(Input.Password)`
+  border-radius: 30px;
+  height: 45px;
+  font-size: 24px;
+  border-color: #a2b4fa;
+`;
+
+export const InputConfirmPassword = styled(Input.Password)`
+  border-radius: 30px;
+  height: 45px;
+  font-size: 24px;
+  border-color: #a2b4fa;
+  margin-left: 8px;
+  width: 98%;
+`;
+
+export const FormItem = styled(Form.Item)`
+  margin-top: 25px;
+  .ant-form-item-label > label {
+    padding-top: 12px;
+    padding-right: 10px;
+    font-weight: bolder;
+    font-size: 16px;
+  }
+  .ant-input {
+    font-size: 18px;
+  }
+`;
+export const ButtonRegister = styled(Button)`
+  border-radius: 30px;
+  background-color: #6adb89;
+  color: white;
+  height: 50px;
+  font-size: 16px;
+  margin: 0 auto;
+  display: block;
+  width: 213px;
+  font-weight: bolder;
   font-size: 20px;
-  margin-bottom: 20px;
 `;
 
 interface IFormInput {
@@ -33,8 +117,8 @@ interface IFormInput {
 type Props = {};
 
 function RegisterPage(props: Props) {
-  const dispatch = useAppDispatch()
-  const history = useHistory()
+  const dispatch = useAppDispatch();
+  const history = useHistory();
   const onFinish = async (values: IFormInput) => {
     const res = await register(
       values.username,
@@ -44,102 +128,99 @@ function RegisterPage(props: Props) {
       values.lastname,
       values.nickname,
       values.birthday,
-      values.gender,
-    )
+      values.gender
+    );
 
-    if(res) {
-      dispatch(setCredentials())
-      history.push('/')
+    if (res) {
+      dispatch(setCredentials());
+      history.push("/");
     }
   };
 
   return (
     <Container>
       <Title>Register</Title>
-      <Form
-        name="basic"
-        onFinish={onFinish}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input />
-        </Form.Item>
+      <BoxForm>
+        <Form name="basic" onFinish={onFinish} autoComplete="off">
+          <FormItem
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <InputForm placeholder="Username" />
+          </FormItem>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
+          <FormItem
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <InputPassword placeholder="Password" />
+          </FormItem>
 
-        <Form.Item
-          label="Confirm"
-          name="confirmPassword"
-          rules={[
-            { required: true, message: "Please input confirm password!" },
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
+          <FormItem
+            label="Confirm"
+            name="confirmPassword"
+            rules={[
+              { required: true, message: "Please input confirm password!" },
+            ]}
+          >
+            <InputConfirmPassword placeholder="Confirm password" />
+          </FormItem>
 
-        <Form.Item
-          label="Firstname"
-          name="firstname"
-          rules={[{ required: true, message: "Please input your firstname!" }]}
-        >
-          <Input />
-        </Form.Item>
+          <FormItem
+            label="Firstname"
+            name="firstname"
+            rules={[
+              { required: true, message: "Please input your firstname!" },
+            ]}
+          >
+            <InputForm placeholder="Firstname" />
+          </FormItem>
 
-        <Form.Item
-          label="Lastname"
-          name="lastname"
-          rules={[{ required: true, message: "Please input your lastname!" }]}
-        >
-          <Input />
-        </Form.Item>
+          <FormItem
+            label="Lastname"
+            name="lastname"
+            rules={[{ required: true, message: "Please input your lastname!" }]}
+          >
+            <InputForm placeholder="Lastname" />
+          </FormItem>
 
-        <Form.Item
-          label="Nickname"
-          name="nickname"
-          rules={[{ required: true, message: "Please input your nickname!" }]}
-        >
-          <Input />
-        </Form.Item>
+          <FormItem
+            label="Nickname"
+            name="nickname"
+            rules={[{ required: true, message: "Please input your nickname!" }]}
+          >
+            <InputForm placeholder="Nickname" />
+          </FormItem>
 
-        <Form.Item
-          label="Birthday"
-          name="birthday"
-          rules={[{ required: true, message: "Please input your birthday!" }]}
-        >
-          <DatePicker format={'MM-DD-YYYY'} />
-        </Form.Item>
+          <FormItem
+            label="Birthday"
+            name="birthday"
+            rules={[{ required: true, message: "Please input your birthday!" }]}
+          >
+            <DatePicker format={"MM-DD-YYYY"} />
+          </FormItem>
 
-        <Form.Item
-          label="Gender"
-          name="gender"
-        >
-          <Radio.Group>
-            <Radio value="male"> male </Radio>
-            <Radio value="female"> female </Radio>
-            <Radio value="LGBTQA+"> LGBTQA+ </Radio>
-          </Radio.Group>
-        </Form.Item>
+          <FormItem label="Gender" name="gender">
+            <Radio.Group>
+              <Radio value="male"> male </Radio>
+              <Radio value="female"> female </Radio>
+              <Radio value="LGBTQA+"> LGBTQA+ </Radio>
+            </Radio.Group>
+          </FormItem>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 8 }}>
-          <Link to="login">Login ?</Link>
-        </Form.Item>
+          <FormItem>
+            <Link style={{ fontSize: "20px" }} to="login">
+              Login ?
+            </Link>
+          </FormItem>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 8 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+          <FormItem>
+            <ButtonRegister htmlType="submit">Submit</ButtonRegister>
+          </FormItem>
+        </Form>
+      </BoxForm>
     </Container>
   );
 }
